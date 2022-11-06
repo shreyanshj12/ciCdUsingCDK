0782d497cb67dcd6af052fe35dbdaea3596daac9
babita.rauniyar <babita.rauniyar@frgrisk.com>
2022-11-05 22:44:01 -0400

    Create Pipeline Stack

---
+++ lib/ci_cd_using_cdk-stack.ts
@@ -0,0 +1,26 @@
+import * as cdk from "aws-cdk-lib";
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from "aws-cdk-lib/pipelines";
+import { Construct } from "constructs";
+import { Env } from "./config/pipeline-config";
+
+export interface CiCdUsingCdkStackProps extends cdk.StackProps {
+  readonly env: Env;
+}
+
+export class CiCdUsingCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: CiCdUsingCdkStackProps) {
+    super(scope, id, props);
+
+    const pipeline = new CodePipeline(this, "Pipeline", {
+      pipelineName: "TestPipeline",
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub("shreyanshj12/ciCdUsingCdk", "main"),
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
+    });
+  }
+}
